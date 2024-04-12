d305dee266946e5b6b9c0a37f863980a078e5b62
majkrone <maciek@ukad.pl>
2024-04-12 13:51:56 +0200

    Zbawy zmiennymi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// liczby calkowite
+
+int myAge2 = 120;
+int newAge2 = myAge2 + 5;
+Console.WriteLine(newAge2);
+int myVar = int.MinValue;
+uint myVar2 = uint.MaxValue;
+ulong myVar3 = ulong.MaxValue;
+
+// liczby zmiennoprzecinkowe
+float myNumber = float.MaxValue;
+double myNumber2 = double.MaxValue;
+
+//zmienne tekstowe
+string name = "Maciek";
+string surname = "Goralczyk";
+string result = name + surname + myAge2;
+Console.WriteLine(result);
+char myVar5 = 'c';
+var result2 = name.ToArray();
