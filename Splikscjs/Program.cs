deeb785b2c424ebfa51c350710f8e3bb94fd3f7a
majkrone <maciek@ukad.pl>
2024-04-12 15:59:56 +0200

    powiekszenie

---
+++ Splikscjs/Program.cs
@@ -0,0 +1,40 @@
+﻿// liczby calkowite
+
+int myAge2 = 120;
+int newAge2 = myAge2 + 5;
+Console.WriteLine(newAge2);
+int myVar = int.MinValue;
+uint myVar2 = uint.MaxValue;
+ulong myVar3 = ulong.MaxValue;
+
+// liczby zmiennoprzecinkowe
+float myNumber = float.MaxValue;
+double myNumber2 = double.MaxValue;
+
+//zmienne tekstowe
+string name = "Maciek";
+string surname = "Góralczyk";
+string result = name + surname + myAge2;
+Console.WriteLine(result);
+char myVar5 = 'c';
+var result2 = name.ToArray();
+
+//zmienna logiczna
+bool isActive = true;
+isActive = false;
+var isValid = 5 > 6;
+Console.WriteLine(isValid);
+
+//instrukcja if i podstawy debugowania
+int number1 = 2;
+int number2 = 10;
+
+if (number1 < number2)
+{
+Console.WriteLine("JESTEM TUTAJ W LINII 34");
+}
+else
+{
+    Console.WriteLine("JESTEM TUTAJ W LINII 38");
+
+}
